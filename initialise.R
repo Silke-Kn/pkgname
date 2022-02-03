e6a3cabd402ea0e4d5916580881c6fa862dcc6f5
sprocketsullivan <silke.kniffert@charite.de>
2022-02-03 08:58:12 +0000

    Initial commit

--- initialise.R
+++ initialise.R
@@ -1,2 +1,3 @@
 install.packages("devtools")
 devtools::install_github("https://github.com/benmarwick/rrtools")
+rrtools::use_compendium("pkgname")
--- pkgname/Setup_comp.R
+++ pkgname/Setup_comp.R
@@ -1,9 +1,9 @@
 #in the following we use the excellent usethis package
 #with this package git interaction works like a charm
 #set a license
-usethis::use_mit_license(copyright_holder = "Ulfunkel")
+usethis::use_mit_license(copyright_holder = "Silke")
 #now we setup git without the terminal
-usethis::use_git_config(user.name = "sprocketsullivan", user.email = "toelch@gmail.com")
+usethis::use_git_config(user.name = "sprocketsullivan", user.email = "silke.kniffert@charite.de")
 #this is a git init
 usethis::use_git()
 #now we create a token on git
@@ -13,7 +13,7 @@ usethis::create_github_token()
 gitcreds::gitcreds_set()
 #now we create a repo called project push to a repository
 usethis::use_github(private = FALSE)
-#create a readme file and edit later 
+#create a readme file and edit later
 rrtools::use_readme_rmd()
 #create and analysis folder
 #data is not shared on github
